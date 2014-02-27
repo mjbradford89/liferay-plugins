51467cf3504f10ccc5728bb5f26dab5fd86b52bc
michael.bradford@liferay.com <michael.bradford@liferay.com>
2014-02-26 16:17:09 -0800

    Fixed issues with broken theme and added the requested functionality

--- themes/broken-theme/docroot/_diffs/js/main.js
+++ themes/broken-theme/docroot/_diffs/js/main.js
@@ -7,15 +7,15 @@ AUI().ready(
 			navigation.plug(Liferay.NavigationInteraction);
 		}
 
-		var siteBreadcrumb = A.one('#breadcrumbs');
+		var siteBreadcrumbs = A.one('#breadcrumbs');
 
 		if (siteBreadcrumbs) {
 			siteBreadcrumbs.plug(A.Hudcrumbs);
 		}
 
-		A.getBody().delegate('click', eventHanler, 'a.logo');
+		A.getBody().delegate('click', eventHandler, 'a.logo');
 
-		var eventHandler function(event) {
+		var eventHandler = function(event) {
 			event.preventDefault();
 			alert(event.currentTarget.attr('title'));
 		}
@@ -24,6 +24,56 @@ AUI().ready(
 
 		if (signIn && signIn.getData('redirect') !== 'true') {
 			signIn.plug(Liferay.SignInModal);
+		}
+
+		var liferayFooterEventHandler = function(event) {
+			event.preventDefault();
+			var modal = new A.Modal(
+				{
+					bodyContent: "<iframe style='display:block; height: 94%; margin:auto; width: 95%;' src='http://www.liferay.com'></iframe>",
+					centered: true,
+					destroyOnHide: false,
+					headerContent: '<h3>Liferay.com</h3>',
+					height: '80%',
+					modal: true,
+					render: '#modal',
+					visible: true,
+					width: '80%'
+				}
+			).render();
+		}
+
+		A.getBody().delegate('click', liferayFooterEventHandler, '#footer .liferay-link');
+
+		var guestBreadcrumbs = A.all('#breadcrumbs.not-signed-in li > a');
+
+		if(guestBreadcrumbs.size() > 0){
+			guestBreadcrumbs.removeAttribute('href');
+
+			var breadcrumbHandler = function (event) {
+				event.preventDefault();
+				A.io.request('/widget/web/guest/home/-/58',
+					{
+						on: {
+							success: function() {
+								var data = this.get('responseData');
+								var modal = new A.Modal({
+										bodyContent: data,
+										centered: true,
+										destroyOnHide: false,
+										headerContent: '<h3>Sign In</h3>',
+										height: 500,
+										render: '#modal',
+										width: 500
+									}
+								).render();
+							}
+						}
+					}
+				);
+			};
+
+			A.getBody().delegate('click', breadcrumbHandler, '#breadcrumbs.not-signed-in li > a');
 		}
 	}
 );
